8dd2b0e082ea1eb8a5e7eec64540c287827f663b
Olegansv <Oleganosv@gmail.com>
2018-01-12 23:32:02 +0200

    Add files via upload

---
+++ serial_init/serial_init/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace serial_init
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (SerialPort serialPort = new SerialPort(args[0], Convert.ToInt32(args[1])))
+                {
+                    serialPort.Open();
+                    serialPort.Close();
+                }
+            }
+            catch { }
+        }
+    }
+}
