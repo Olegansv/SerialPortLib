4060c30a6911489266ebd9050aa71208f8a320a0
Olegansv <Oleganosv@gmail.com>
2018-01-12 23:28:45 +0200

    Update serial.py

--- serial.py
+++ serial.py
@@ -1,63 +1,41 @@
 import os
 from subprocess import call
 
-__isOpened = False
+class SerialPort:
+    __port = None
+    __baudRate = None
+    __serial = None
+    __logging = True
+    __isOpened = False
+    
+    def __init__(self, port = None, baudRate = None, logging = True):
+        self.__port = port
+        self.__baudRate = baudRate
+        self.__logging = logging
+    
+    def init(self):
+        call(["serial_init.exe", str(self.__port), str(self.__baudRate)])
 
-def s_init(port, baudRate):
-	try:
-		call(["serial_init.exe", str(port), str(baudRate)])
-	except WindowsError:
-		print("Error: can't open \"serial_init.exe\"")
+    def open(self):
+        if self.__isOpened:
+            raise OSError("Port %s already opened" % (self.__port))
+        else:
+            self.__serial = os.open(self.__port, os.O_RDWR)
+            self.__isOpened = True
+            if self.__logging:
+                print("Port %s opened" % (self.__port))
+    
+    def setPort(self, port):
+        if not self.__isOpened:
+            self.__port__ = port
+    
+    def setBaudRate(self, baudRate):
+        if not self.__isOpened:
+            self.__baudRate = baudRate
+    
+    def isOpened(self):
+        return self.__isOpened
 
-def s_open(port):
-	global __serial, __isOpened
-	if not __isOpened:
-		try:
-			__serial = os.open(port, os.O_RDWR)
-		except:
-			print("Can't open port %s" % (port))
-			__isOpened = False
-		else:
-			print("Port %s opened" % (port))
-			__isOpened = True
-	elif __isOpened:
-		print("Port already opened")
-
-def s_close():
-	global __isOpened
-	if __isOpened:
-		print("Port closed")
-		os.close(__serial)
-		__isOpened = False
-
-def s_ports(lim = 10):
-	ports = []
-	for i in range(1, lim + 1):
-		try:
-			__port = os.open("COM%s" % (i), os.O_RDWR)
-			os.close(__port)
-		except OSError as e:
-			if e.errno == 13: # PermissionError
-				ports.append({"COM%s" % (i): "busy"})
-		except:
-			continue
-		else:
-			ports.append({"COM%s" % (i): "available"})
-	return ports
-
-def s_read(lastSymbol = '\n'):
-	buffer = ""
-	if __isOpened:
-		while True:
-			received = os.read(__serial, 1024).decode()
-			buffer += received
-			if lastSymbol in buffer:
-				break
-		return buffer
-
-def s_send(text):
-	if __isOpened:
-		os.write(__serial, text)
-
-def s_isOpened():
-	return __isOpened
+    def send(self, text):
+        if self.__isOpened:
+            os.write(self.__serial, text)
